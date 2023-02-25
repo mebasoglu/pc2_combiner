cda429c7790bcafc5013e4cf0e679a024c11fcc1
Mehmet Emin BAŞOĞLU <mehmeteminbasoglu@hotmail.com>
2023-02-25 23:51:51 +0300

    Get transforms

--- include/pc2_combiner/PC2Combiner.hpp
+++ include/pc2_combiner/PC2Combiner.hpp
@@ -1,15 +1,50 @@
 #pragma once
 
 #include "rclcpp/rclcpp.hpp"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
 
 namespace pc2_combiner
 {
 class PC2Combiner : public rclcpp::Node
 {
   public:
-    /*!
-     * Constructor.
+    /**
+     * @brief Constructor of the main node class.
      */
     explicit PC2Combiner();
+
+  private:
+    /**
+     * @brief Waits for the transform to be available and gets it.
+     */
+    void getTransform();
+
+    /**
+     * @brief Checks if the transform is ready.
+     * 
+     * @return true if the transform is available, otherwise false.
+     */
+    bool isTransformAvailable() const;
+
+    void topic_callback(const sensor_msgs::msg::PointCloud2& msg) const;
+
+
+    // Transform tools.
+    tf2_ros::Buffer m_tf_buffer;
+    tf2_ros::TransformListener m_tf_listener;
+
+    // Stores the transform for velodyne_left.
+    geometry_msgs::msg::TransformStamped m_tf_left;
+
+    // Stores the transform for velodyne_right.
+    geometry_msgs::msg::TransformStamped m_tf_right;
+    
+    // Stores the transform for velodyne_top.
+    geometry_msgs::msg::TransformStamped m_tf_top;
+
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
 };
 }  // namespace pc2_combiner
--- src/PC2Combiner.cpp
+++ src/PC2Combiner.cpp
@@ -2,8 +2,60 @@
 
 namespace pc2_combiner
 {
-PC2Combiner::PC2Combiner() : Node{ "pc2_combiner" }
+PC2Combiner::PC2Combiner()
+  : Node{ "pc2_combiner" }, m_tf_buffer{ get_clock() }, m_tf_listener{ m_tf_buffer }
 {
-    RCLCPP_INFO(get_logger(), "Hello World!");
+    RCLCPP_INFO(get_logger(), "Starting %s node.", get_name());
+
+    getTransform();
+
+    subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+        "/sensing/lidar/left/pointcloud_raw", rclcpp::SensorDataQoS(),
+        std::bind(&PC2Combiner::topic_callback, this, std::placeholders::_1));
+
+    RCLCPP_INFO(get_logger(), "Initialized %s node.", get_name());
+}
+
+void PC2Combiner::getTransform()
+{
+    try
+    {
+        rclcpp::Rate wait_rate{ 1 };
+        while (!isTransformAvailable() && rclcpp::ok())
+        {
+            // Wait for the transform to become available.
+            RCLCPP_INFO(get_logger(), "Waiting for transform.");
+            wait_rate.sleep();
+        }
+        // Store transforms.
+        m_tf_left = m_tf_buffer.lookupTransform("base_link", "velodyne_left", tf2::TimePointZero);
+        m_tf_right = m_tf_buffer.lookupTransform("base_link", "velodyne_right", tf2::TimePointZero);
+        m_tf_top = m_tf_buffer.lookupTransform("base_link", "velodyne_top", tf2::TimePointZero);
+    }
+    catch (const tf2::TransformException& ex)
+    {
+        RCLCPP_INFO(get_logger(), "Error. %s", ex.what());
+    }
+}
+
+bool PC2Combiner::isTransformAvailable() const
+{
+    const bool tf_left{ m_tf_buffer.canTransform("base_link", "velodyne_left",
+                                                 tf2::TimePointZero) };
+
+    const bool tf_right{ m_tf_buffer.canTransform("base_link", "velodyne_right",
+                                                  tf2::TimePointZero) };
+
+    const bool tf_top{ m_tf_buffer.canTransform("base_link", "velodyne_top", tf2::TimePointZero) };
+
+    return tf_left && tf_right && tf_top;
+}
+
+void PC2Combiner::topic_callback(const sensor_msgs::msg::PointCloud2& msg) const
+{
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.header.frame_id.c_str());
+    RCLCPP_INFO(get_logger(), "%f", m_tf_left.transform.translation.z);
+    RCLCPP_INFO(get_logger(), "%f", m_tf_right.transform.translation.z);
+    RCLCPP_INFO(get_logger(), "%f", m_tf_top.transform.translation.z);
 }
 }  // namespace pc2_combiner
